8151127cf93380490697057a4b3524056886378c
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-05 18:34:39 -0400

    Configured dependencies

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// Set up Express app
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static("./app/public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Require routes for API endpoints and HTML views
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// Start the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
