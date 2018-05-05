064d7fb03d5144f20ebb895880a6bf9b2ccda120
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-04 22:49:50 -0400

    Setting up dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+// Require dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
