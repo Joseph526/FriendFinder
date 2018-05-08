081e91a2c7a099ecb982d485671b403d20f327bb
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-07 21:48:23 -0400

    Test comparison against one existing user

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,15 @@ module.exports = function(app) {
 
     // POST route to update friends
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
+        var newUser = req.body;
+        
+        // Compare against one existing user to test
+        var totalDifference = 0;
+        for (var i = 0; i < friendsData[0].scores.length; i++) {
+            totalDifference += Math.abs(newUser.scores[i] - friendsData[0].scores[i]);
+        }
+        console.log("TOTAL DIFFERENCE: " + totalDifference);
+        friendsData.push(newUser);
         res.end();
     });
 };
